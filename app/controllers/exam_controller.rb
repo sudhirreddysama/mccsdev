f6012faf46606f87d86781060b6ffd87a3ae7442
root <root@WebCivilService.(none)>
2015-09-28 14:51:14 -0400

    Special accommodations report

--- app/controllers/exam_controller.rb
+++ app/controllers/exam_controller.rb
@@ -242,6 +242,23 @@ class ExamController < CrudController
 					
 					@print_orient = 'Landscape'
 				
+				elsif params[:special_accommodations]
+				
+					cond << 'wa.special_accommodations = 1 or p.special_accommodations != "" or p.sabbath_observer = 1'
+				
+					objs = DB.query('
+						select p.first_name, p.last_name, p.ssn, p.special_accommodations, p.sabbath_observer, p.special_accommodations_docs, p.sabbath_observer_docs, exams.exam_no, exams.given_at, wa.special_accommodations web_special_accommodations
+						from applicants a
+						join people p on p.id = a.person_id
+						left join ' + HRAPPLYDB + '.applicants wa on wa.id = a.web_applicant_id
+						join exams on exams.id = a.exam_id
+						where ' + get_where(cond) + '
+						order by p.last_name asc, p.first_name asc'
+					)
+					@objs = []
+					objs.each_hash { |h| @objs << h }
+					@print_orient = 'Landscape'
+				
 				elsif params[:contact_preference]
 					@objs = DB.query('
 						select
