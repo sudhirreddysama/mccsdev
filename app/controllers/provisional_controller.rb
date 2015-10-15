ef58c4b16feeb8f350757d0c10bbfc54685bd95a
root <root@WebCivilService.(none)>
2015-10-15 10:57:37 -0400

    Added sort/search options to provisionals tab

--- app/controllers/provisional_controller.rb
+++ app/controllers/provisional_controller.rb
@@ -12,12 +12,26 @@ class ProvisionalController < CrudController
 			['Title', 'provisionals.title'],
 			['Name', 'provisionals.name'],
 			['Jurisdiction', 'provisionals.jurisdiction'],
+			['2nd Prov', 'provisionals.second_provisional'],
+			['2nd Prov (as date)', 'str_to_date(provisionals.second_provisional, "%m/%d/%Y")'],
+			['Exam Req.', 'provisionals.exam_requested'],
+			['Exam Req. (as date)', 'str_to_date(provisionals.exam_requested, "%m/%d/%Y")'],
+			['Orig. Apt.', 'provisionals.original_appointment'],
+			['Orig. Apt. (as date)', 'str_to_date(provisionals.original_appointment, "%m/%d/%Y")'],
+			['Exam Date', 'provisionals.exam_date'],
+			['Exam Date (as date)', 'str_to_date(provisionals.exam_date, "%m/%d/%Y")'],
+			['Ref. Date (as date)', 'provisionals.reference_date'],
+			['Illegal', 'provisionals.pass_fail']
 		]
 		cond = get_search_conditions @filter[:search], {
 			'provisionals.id' => :left,
 			'provisionals.title' => :like,
 			'provisionals.name' => :like,
 			'provisionals.jurisdiction' => :like,
+			'provisionals.second_provisional' => :like,
+			'provisionals.exam_requested' => :like,
+			'provisionals.original_appointment' => :like,
+			'provisionals.exam_date' => :like
 		}
 		
 		@opt = {
