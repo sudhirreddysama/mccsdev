4655ed643bef2a8ed1cf6a92937833713123105c
root <root@WebCivilService.(none)>
2016-03-10 15:02:24 -0500

    Added new user permission + omb liaisons

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -8,6 +8,7 @@ class Agency < ActiveRecord::Base
   has_many :agency_jobs
   has_many :jobs, :through => :agency_jobs
 	belongs_to :liaison, :class_name => 'User', :foreign_key => 'liaison_id'
+	belongs_to :omb_liaison, :class_name => 'User', :foreign_key => 'omb_liaison_id'
 	
 	validates_presence_of :name
 	
@@ -28,6 +29,20 @@ class Agency < ActiveRecord::Base
 			end
 		end
 	end
+	
+	def self.get_omb_liaison a, d
+		if a.nil? && d
+			return d.omb_liaision
+		elsif d.nil? && a
+			return a.omb_liaison
+		elsif a && d
+			if d.omb_liaison && a.use_dept_liaison
+				return d.omb_liaison
+			else
+				return a.omb_liaison
+			end
+		end
+	end	
 	
 	def get_users d
 		# if no department, just find everyone with this agency_id who also has no department
--- app/models/department.rb
+++ app/models/department.rb
@@ -3,6 +3,7 @@ class Department < ActiveRecord::Base
 	belongs_to :agency
 	
 	belongs_to :liaison, :class_name => 'User', :foreign_key => 'liaison_id'
+	belongs_to :omb_liaison, :class_name => 'User', :foreign_key => 'omb_liaison_id'
 	
 	validates_presence_of :name
 	
