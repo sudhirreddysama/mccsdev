939037f539e5466037394017a0d105ababb7f3c6
root <root@WebCivilService.(none)>
2017-01-09 09:41:52 -0500

    Fixes to vacancy data import

--- app/models/vacancy_data.rb
+++ app/models/vacancy_data.rb
@@ -8,25 +8,27 @@ class VacancyData < ActiveRecord::Base
 		ftp = Net::FTP.new '10.100.224.234'
 		ftp.login 'civilservice', 's@prtf123'
 		data = nil
-		# Just schedule the cron job a good time after the SAP dump. The modified time reported by FTP is unreliable. Timezone issue.
-		#mtime = ftp.mtime 'positon.txt', true
-		#if mtime.advance(:minutes => 10) < Time.now
-			logger.info 'Loading File'
-			objs = []
-			ftp.gettextfile('positon.txt', 'tmp/positon.txt') { |line|
-				parts = line.unpack('A8x45A40x8AA8A40A10x2A6x37A2A*') rescue nil
-				if parts
-					d = VacancyData.new
-					d.position_no, d.position, d.status, d.org_no, d.organization, d.cost_center, d.job_no, d.salary_group, d.last_incumbent = parts
-					objs << d
-				end
-			}
-			if !objs.empty?
-				logger.info "Saving Records, Count: #{objs.size}"
-				VacancyData.delete_all
-				objs.each &:save
+		logger.info 'Loading File'
+		objs = []
+		first = true
+		ftp.gettextfile('positon.txt', 'tmp/positon.txt') { |line|
+			if first
+				first = false
+				next
 			end
-		#end
+			line = line.ljust(207) # Minimum length, right most fields not required.
+			parts = line.unpack('A8x45A40x8AA8A40A10x2A6x37A2A*') rescue nil
+			if parts
+				d = VacancyData.new
+				d.position_no, d.position, d.status, d.org_no, d.organization, d.cost_center, d.job_no, d.salary_group, d.last_incumbent = parts
+				objs << d
+			end
+		}
+		if !objs.empty?
+			logger.info "Saving Records, Count: #{objs.size}"
+			VacancyData.delete_all
+			objs.each &:save
+		end
 		ftp.close
 		logger.info "VacancyData.cron finished #{Time.now}"
 	end
