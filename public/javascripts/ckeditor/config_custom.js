c67aff45e79398cd5c6d3bb3af282f6faf4533c1
root <root@WebCivilService.(none)>
2017-05-05 12:31:29 -0400

    Added exhausted field

--- public/javascripts/ckeditor/config_custom.js
+++ public/javascripts/ckeditor/config_custom.js
@@ -7,7 +7,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-	allowedContent: true,
 	config.extraPlugins = 'scayt';
 	
 	config.toolbarGroups = [
