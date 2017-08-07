033b0bba16be5a9415fd6348d4a8c7175c410fa9
root <root@WebCivilService.(none)>
2017-08-07 10:55:14 -0400

    DOT Letterhead

--- app/models/message.rb
+++ app/models/message.rb
@@ -33,9 +33,10 @@ class Message < ActiveRecord::Base
 			foot = 'letter-footer.html'
 			if letterhead == 'dhs'
 				foot = 'letter-footer-no-address.html'
-			end
-			if letterhead == 'des'
+			elsif letterhead == 'des'
 				foot = 'letter-footer-des.html'
+			elsif letterhead == 'dot'
+				foot = 'letter-footer-dot.html'				
 			end
 			`wkhtmltopdf --disable-smart-shrinking --footer-html /home/rails/mccs#{RAILS_ENV == 'development' ? 'dev' : ''}/#{foot} -s Letter -O Portrait --margin-left 1in --margin-right 1in --margin-top .5in --margin-bottom .5in #{f.path} #{path}`			
 			update_attribute :rendered_pdf, true
