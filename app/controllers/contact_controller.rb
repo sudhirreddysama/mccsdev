227d7c11e0fffe6204511f7cbc01cb14dbb088cb
root <root@WebCivilService.(none)>
2018-02-14 13:48:25 -0500

    agency contact export

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,24 +2,25 @@ class ContactController < CrudController
 
   def index
     @filter = get_filter({
-                             :sort1 => 'contacts.lastname',
-                             :dir1 => 'desc'
-                         })
+			:sort1 => 'contacts.lastname',
+			:dir1 => 'desc'
+		})
     @orders = [
-        ['Last Name', 'contacts.lastname'],
-        ['First Name', 'contacts.firstname'],
-        ['Department', 'departments.name']
+			['Last Name', 'contacts.lastname'],
+			['First Name', 'contacts.firstname'],
+			['Department', 'departments.name']
     ]
     cond = get_search_conditions @filter[:search], {
-        'contacts.lastname' => :like,
-        'contacts.firstname' => :like,
-        'departments.name' => :like
+			'contacts.lastname' => :like,
+			'contacts.firstname' => :like,
+			'departments.name' => :like
     }
     @opt = {
-        :conditions => get_where(cond),
-        :order => get_order_auto,
-        :include => :department,
+			:conditions => get_where(cond),
+			:order => get_order_auto,
+			:include => :department,
     }
+		@export_fields = %w{id agency_id agency.name lastname firstname title email phone fax primary department_id department.name organization_name address address2 city state zip provisional_contact}
     super
   end
 
