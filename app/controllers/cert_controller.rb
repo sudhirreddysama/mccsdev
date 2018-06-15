38a957d7ed63088c808cb9198c7ee4dd8f062bfa
root <root@WebCivilService.(none)>
2018-06-15 09:21:07 -0400

    nav and cache buster

--- app/controllers/cert_controller.rb
+++ app/controllers/cert_controller.rb
@@ -216,28 +216,7 @@ class CertController < CrudController
 	
 	def check_other_certs
 		if @ca.applicant.app_status && @ca.applicant.app_status.appointed && @obj.completed_date.nil?
-			CertApplicant.appointed_from_other_cert_cron @ca.id
-# 			logger.info "Checking other certs, from cert ID: #{@ca.cert_id}"
-# 			cond = ['ifnull(cert_codes.description, "") = ""', 'certs.certification_date is not null', 'certs.completed_date is null']
-# 			cond << 'certs.id != %d' % @ca.cert_id
-# 			if @obj.exam.continuous && @obj.exam.current_exam_id
-# 				cond << ('exams.current_exam_id = %d' % @obj.exam.current_exam_id)
-# 			else
-# 				cond << ('exams.id = %d' % @obj.exam_id)
-# 			end		
-# 			cond << 'applicants.person_id = %d' % @ca.applicant.person_id
-# 			CertApplicant.find(:all, {
-# 				:include => [{:cert => :exam}, :applicant, :cert_code], 
-# 				:conditions => get_where(cond)
-# 			}).each { |ca|
-# 				logger.info "Setting cert code for cert ID #{ca.cert_id}"
-# 				users = ca.cert.agency ? ca.cert.agency.get_users(ca.cert.department) : []
-# 				ca.cert_code = CertCode.find_by_code 'AOA'
-# 				ca.save
-# 				if !users.empty?
-# 					Notifier.deliver_cert_applicant_appointed users, ca, @ca
-# 				end
-# 			}
+			#CertApplicant.appointed_from_other_cert_cron @ca.id
 		end
 	end
 	
